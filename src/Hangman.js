abe111187d5aa4adb2f62951314671871ca9d7c2
cdsalty <soltiscd@gmail.com>
2019-06-06 16:05:11 -0400

    added key attributes and assigned to the letter guessed

--- src/Hangman.js
+++ src/Hangman.js
@@ -46,10 +46,11 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
-      >
+        >
         {ltr}
       </button>
     ));
